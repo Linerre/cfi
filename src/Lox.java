bd061841d7694e1b9cf5aa99ebce77c39855c043
Linerre <leon.errelin@outlook.com>
2024-06-17 17:10:29 +1000

    Finish scanning (chapter 4)

--- src/Lox.java
+++ src/Lox.java
@@ -1,4 +1,4 @@
-package craftingintepresters.lox;
+package craftinginterpreters.lox;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
     static boolean hadError = false;
@@ -40,7 +39,7 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
-            hadError(false);
+            hadError = false;
         }
     }
 
